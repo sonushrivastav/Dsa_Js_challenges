38e8751f88ccada8531867f39cf7c55ca146ec23
Mern-stack-developer <sonushrivastav1811@gmail.com>
2024-05-16 21:08:24 +0530

    remove suplicates from an array  code

---
+++ basic_challenge_1/10_remove_duplicates/remove_duplicate.js
@@ -0,0 +1,17 @@
+function removeDuplicate(arr){
+   let uniqueArr = []
+
+   for(let i=0;i<arr.length;i++){
+     if(!uniqueArr.includes(arr[i])){
+        uniqueArr.push(arr[i])
+     }
+   }
+
+   return uniqueArr
+}
+
+function removeDuplicate(arr){
+    return Array.from(new Set(arr))
+}
+
+module.exports = removeDuplicate;
---
+++ basic_challenge_1/10_remove_duplicates/remove_duplicate.run.js
@@ -0,0 +1,4 @@
+const removeDuplicate = require("./remove_duplicate")
+
+const result = removeDuplicate([1,4,9,3,0,3,5,3])
+console.log(result);
