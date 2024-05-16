cd83a5eaffa5edf815469c6c598933f60dde1d1d
Mern-stack-developer <sonushrivastav1811@gmail.com>
2024-05-16 20:07:24 +0530

    added revers string code

---
+++ basic_challenge_1/07_reverse_string/reverse_string.js
@@ -0,0 +1,13 @@
+// const reverseString = (str) => str.split("").reverse().join("")
+
+function reverseString(str){
+    let reversed = ""
+
+    for(let i=str.length-1;i>=0;i--){
+     reversed += str[i]
+    }
+
+    return reversed
+}
+
+module.exports = reverseString;
