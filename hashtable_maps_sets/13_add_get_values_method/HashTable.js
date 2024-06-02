652c3b188d52bbc9a497b428543b9fb1ca22917e
Mern-stack-developer <150647611+sonushrivastav123@users.noreply.github.com>
2024-06-02 21:28:49 +0530

    added getValues from hashTable

---
+++ hashtable_maps_sets/13_add_get_values_method/HashTable.js
@@ -0,0 +1,114 @@
+class HashTable {
+    constructor(limit =14){
+        this.storage = []
+        this.limt = limit
+    }
+
+     // hash function
+    _hash(key,max){
+        let hash = 0
+        for (let i=0;i<key.length;i++){
+            hash += key.charCodeAt(i)
+        }
+
+        return hash % max
+    }
+
+    // insert a key value pair into hash table
+    set(key,value){
+        const index = this._hash(key,this.limt)
+
+        if(this.storage[index] === undefined){
+            this.storage[index] = [[key,value]]
+        }else{
+            let inserted = false
+
+            for(let i =0; i<this.storage[index].length; i++){
+                if(this.storage[index][i][0] === key){
+                    this.storage[index][i][1] = value
+                    inserted = true
+                }
+            }
+
+            if(inserted === false){
+                this.storage[index].push([key,value])
+            }
+        }
+    }
+
+    printTable(){
+      for(let i = 0; i < this.storage.length;i++){
+        if(this.storage[i] !== undefined){
+            console.log(`Bucket ${i}: ${JSON.stringify(this.storage[i])}`);
+        }else{
+            console.log(`Bukcet ${i} Empty`);
+        }
+      }
+    }
+
+    // get a value from hash table
+    get(key){
+      const index = this._hash(key,this.limt)
+
+      if(this.storage[index] === undefined){
+          return undefined
+      }else{
+          for(let i=0; i<this.storage[index].length; i++){
+              if(this.storage[index][i][0] === key){
+                  return this.storage[index][i][1]
+              }
+          }
+      }
+    }
+    // remove a key value pair from hash table
+    remove(key){
+        const index = this._hash(key,this.limt)
+       if(this.storage[index]){
+         if(this.storage[index].length === 1 && this.storage[index][0][0] === key){
+            delete this.storage[index]
+         }else{
+            for(let i =0;i<this.storage[index].length;i++){
+                if(this.storage[index][i][0] === key){
+                    delete this.storage[index][i]
+                    
+                }
+            }
+         }
+       }
+    }
+
+    has(key){
+        const index = this._hash(key,this.limt)
+
+        if(this.storage[index]){
+            for(let i=0; i<this.storage[index].length ;i++){
+                if(this.storage[index][i][0] === key){
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    clear(){
+        this.storage = []
+    }
+
+    getvalues(){
+        const values = []
+    
+        for(let i=0; i<this.storage.length;i++){
+            if(this.storage[i]){
+                for(const [key,value] of this.storage[i]){
+                    values.push(value)
+                }
+            }
+        }
+    
+        return values
+    }
+        
+}
+
+module.exports = HashTable
