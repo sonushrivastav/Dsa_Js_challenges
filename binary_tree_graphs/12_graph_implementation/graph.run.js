52208c099a4b8700960c09923ac7668811e0994f
sonu shrivastav <sonushrivastav1811@gmail.com>
2024-06-16 20:43:19 +0530

    added graph imelemntation

---
+++ binary_tree_graphs/12_graph_implementation/graph.run.js
@@ -0,0 +1,20 @@
+const Graph = require('./graph');
+
+// Create a graph:
+//    Tokyo
+//    /   \
+// Dallas - Aspen
+
+const g = new Graph();
+g.addVertex('Tokyo');
+g.addVertex('Dallas');
+g.addVertex('Aspen');
+g.addEdge('Tokyo', 'Dallas');
+g.addEdge('Dallas', 'Aspen');
+g.addEdge('Aspen', 'Tokyo');
+
+g.removeEdge('Dallas','Aspen')
+
+g.removeVertex('Aspen')
+g.printAdjacencyList()
+
