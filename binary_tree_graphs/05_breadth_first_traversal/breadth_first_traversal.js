8f803474bef4dd0bc08c666409c76b2852aa2d87
sonu shrivastav <sonushrivastav1811@gmail.com>
2024-06-16 16:39:56 +0530

    added breadth first traversal

---
+++ binary_tree_graphs/05_breadth_first_traversal/breadth_first_traversal.js
@@ -0,0 +1,40 @@
+const Queue = require("./queue")
+
+class Node{
+    constructor(data){
+        this.data = data
+        this.left = null
+        this.right = null
+    }
+}
+
+function breadthFirstTraversal(root){
+    if(!root) return []
+
+    const result = []
+    const queue = []
+
+    queue.push(root)
+
+    while(queue.length > 0){
+        const current = queue.shift()
+        result.push(current.data)
+
+        if(current.left){
+            queue.push(current.left)
+        }
+
+        
+
+        if(current.right){
+            queue.push(current.right)
+        }
+    }
+
+    return result
+}
+
+module.exports = {
+    Node,
+    breadthFirstTraversal
+}
