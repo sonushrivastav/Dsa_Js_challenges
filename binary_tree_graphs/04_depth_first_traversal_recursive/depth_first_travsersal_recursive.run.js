2d2492ff112319a988bf706bba4512a640b249fc
sonu shrivastav <sonushrivastav1811@gmail.com>
2024-06-16 16:26:48 +0530

    added depth first traversal recursive

---
+++ binary_tree_graphs/04_depth_first_traversal_recursive/depth_first_travsersal_recursive.run.js
@@ -0,0 +1,18 @@
+const {Node,recDepthFirstTraversal} = require("./depth_first_travsersal_recursive")
+
+const root = new Node("a")
+const nodeB = new Node("b")
+const nodeC = new Node("c")
+const nodeD = new Node("d")
+const nodeE = new Node("e")
+const nodeF = new Node("f")
+
+root.left = nodeB
+root.right = nodeC
+nodeB.left = nodeD
+nodeB.right = nodeE
+nodeC.left = nodeF  
+
+
+const result = recDepthFirstTraversal(root)
+console.log(result);
